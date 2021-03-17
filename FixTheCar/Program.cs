52574f9759398951ce76dc6293ade7faf122f1f9
RickieCanCodeIT <rickie@WeCanCodeIT.org>
2021-03-17 13:56:25 -0400

    Initial commit

---
+++ FixTheCar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FixTheCar
+{
+    class Program
+    {
+        private static Vehicle motorcycle;
+        private static Vehicle unicycle;
+        private static Vehicle redVehicle;
+        private static Vehicle blueVehicle;
+        static void Main(string[] args)
+        {
+            motorcycle = new Vehicle(2, 4f, 20, "Motorcycle");
+            unicycle = new Vehicle(1, 3f, 20, "Unicycle");
+            redVehicle = new Vehicle(4, 5f, 20, "Red Car");
+            while (motorcycle.CanMove())
+            {
+                motorcycle.Move();
+            }
+            while (unicycle.CanMove())
+            {
+                unicycle.Move();
+            }
+            while (redVehicle.CanMove())
+            {
+                redVehicle.Move();
+            }
+            while (blueVehicle.CanMove())
+            {
+                blueVehicle.Move();
+            }
+
+        }
+    }
+}
---
+++ FixTheCar/Vehicle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixTheCar
+{
+    class Vehicle
+    {
+        private int numberOfWheels;
+        private double speed;
+        private int fuel;
+        private bool flatTire;
+        private double distanceTravelled;
+        private string name;
+        public static Random random = new Random();
+
+        public Vehicle(int numberOfWheels, double speed, int fuel, string name)
+        {
+            this.numberOfWheels = numberOfWheels;
+            this.fuel = fuel;
+            this.flatTire = false;
+            this.name = name;
+            this.distanceTravelled = 0;
+        }
+
+        public void Move()
+        {
+            fuel--;
+            if (flatTire)
+            {
+                flatTire = false;
+                Console.WriteLine(name + " has travelled " + distanceTravelled + " feet.");
+            }
+            else
+            {
+                distanceTravelled += speed;
+                Console.WriteLine(name + " has travelled " + distanceTravelled + " feet.");
+                for (int i = 1; i < numberOfWheels; i++)
+                {
+                    if (random.Next(12) == 0)
+                    {
+                        flatTire = true;
+                        Console.WriteLine(name + " has a flat tire.");
+                        break;
+                    }
+                }
+            }
+        }
+
+        public bool CanMove()
+        {
+            if (fuel > 0)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(name + " is out of gas.");
+                return false;
+            }
+        }
+    }
+}
